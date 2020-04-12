995840e0b7cacff9a5eb5fa9dc02101f28e43f85
Nick Whitelegg <nickw4426@gmail.com>
2020-04-12 12:43:00 +0100

    validate params in proxy

--- proxy.php
+++ proxy.php
@@ -2,6 +2,11 @@
 $x = $_GET["x"];
 $y = $_GET["y"];
 $z = $_GET["z"];
-header("Content-Type: image/png");
-echo file_get_contents("https://s3.amazonaws.com/elevation-tiles-prod/terrarium/$z/$x/$y.png");
+if(ctype_digit($x) && ctype_digit($y) && ctype_digit($z)) {
+    header("Content-Type: image/png");
+    echo file_get_contents("https://s3.amazonaws.com/elevation-tiles-prod/terrarium/$z/$x/$y.png");
+} else {
+    header("HTTP/1.1 400 Bad Request");
+    echo "invalid x, y and/or z params";
+}
 ?>
